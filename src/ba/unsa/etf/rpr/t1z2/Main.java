63078475e82cafa7771e6f53979647295753d047
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-08 20:11:20 +0100

    t1-z2: zavrsen zadatak

--- src/ba/unsa/etf/rpr/t1z2/Main.java
+++ src/ba/unsa/etf/rpr/t1z2/Main.java
@@ -1,8 +1,28 @@
 package ba.unsa.etf.rpr.t1z2;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void sumaCifara(int n) {
+        int i = 0;
+        for(i = 1;i <= n;i++) {
+            int broj = i, suma_cifara = 0;
+            while(broj != 0) {
+                suma_cifara += broj % 10;
+                broj /= 10;
+            }
+            if(i % suma_cifara == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite broj n: ");
+        int n = ulaz.nextInt();
+        sumaCifara(n);
 	// write your code here
     }
 }
